0dc059d5b3f0b3fdb9a473196b9e9b653edba5ef
Gabriele Scigliano <31177235+gscigliano@users.noreply.github.com>
2017-09-01 07:02:50 +0100

    Update nx.tcl

--- nx.tcl
+++ nx.tcl
@@ -4,16 +4,23 @@ tclsh
 
 set LOOPS 10	; # how many times
 set INTERFACE "po16"	; # which interface
+set INTERVAL 1000 ; # milliseconds interval - how often you take samples
+
 set OLDOutUcastPkts 0	; # dont touch
+set OLDcurrTimeMS 0	; # dont touch
 
 for {set i 0} {$i < $LOOPS} {incr i} {
  set OutUcastPkts [exec "sh int $INTERFACE counters" ]
- set OutUcastPkts [regexp -inline -all -- {\S+} $OutUcastPkts]
- set OutUcastPkts [lindex $OutUcastPkts 23]
- if {$i ==0 } { puts "unix time [clock seconds] interface $INTERFACE pktsout $OutUcastPkts" 
- } else { 
-  puts "unix time [clock seconds] interface $INTERFACE newpktsout $OutUcastPkts oldpktsout $OLDOutUcastPkts\tPPS is [expr ($OutUcastPkts-$OLDOutUcastPkts)]"
+ set OutUcastPkts [regexp -inline -all -- {\S+} $OutUcastPkts]	; # looking for OutUcastPkts
+ set OutUcastPkts [lindex $OutUcastPkts 23]	
+ set currTimeMS [clock clicks -milliseconds] ; # at what time counters were taken and parsed, in milliseconds to be precise
+
+ if {$i !=0 } { 
+  puts "clock clicks (ms) $currTimeMS interface $INTERFACE newpktsout $OutUcastPkts oldpktsout $OLDOutUcastPkts"
+  puts "UTC time [clock format [clock seconds] -format %H:%M:%S] and PPS is [expr ($OutUcastPkts-$OLDOutUcastPkts)*(1/(($currTimeMS-$OLDcurrTimeMS)/1000.0))]" 
  }
  set OLDOutUcastPkts $OutUcastPkts
- after 1000
+ set OLDcurrTimeMS $currTimeMS
+
+ after $INTERVAL	; # sleep for interval
 }
